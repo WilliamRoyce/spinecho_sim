e1b22fe10761995c79a427353fc5bdc629887cb7
William Royce <wr286@cam.ac.uk>
2025-07-15 11:04:39 +0000

    Fixed all majorana type checking

--- examples/majorana_points.py
+++ examples/majorana_points.py
@@ -3,14 +3,10 @@ from __future__ import annotations
 import numpy as np
 
 from spinecho_sim.majorana import (
-    majorana_points,
     majorana_points_by_index,
     stars_to_state,
 )
 
 if __name__ == "__main__":
-    print(majorana_points([1, 0, 1]))
-    # print(majorana_points([0, 1, 1]))
-    # print("\n", majorana_points_multiple([[1, 0, 1], [0, 1, 1]]))
-    print("\n", majorana_points_by_index([[1, 0, 1], [0, 1, 1]]))
+    print("\n", majorana_points_by_index([np.array([1, 0, 1]), np.array([0, 1, 1])]))
     print("\n", stars_to_state([(np.pi / 2, np.pi / 2), (np.pi / 2, 3 * np.pi / 2)]))
