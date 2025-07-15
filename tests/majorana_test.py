3e12b2514f3bb54fbb5f1b241abc7bd62451a63e
William Royce <wr286@cam.ac.uk>
2025-07-15 13:41:54 +0000

    fixed majorana star data types

--- tests/majorana_test.py
+++ tests/majorana_test.py
@@ -8,7 +8,13 @@ from spinecho_sim.majorana import (
 )
 
 if __name__ == "__main__":
-    spin_states = np.array([[1.0, 0.0, 1.0], [0.0, 1.0, 1.0]], dtype=np.complex128)
+    spin_states = np.array(
+        [
+            [(1.0 + 0.0j) / np.sqrt(2), (0.0 + 0.0j), (1.0 + 0.0j) / np.sqrt(2)],
+            [(0.0 + 0.0j), (1.0 + 0.0j) / np.sqrt(2), (1.0 + 0.0j) / np.sqrt(2)],
+        ],
+        dtype=np.complex128,
+    )
     result = majorana_points_by_index(spin_states)
     print("\n", result)
     multi_stars = np.array(
@@ -16,6 +22,6 @@ if __name__ == "__main__":
             [[np.pi / 2, np.pi / 2], [np.pi / 2, 3 * np.pi / 2]],
             [[np.pi, 0.0], [np.pi / 2, 0.0]],
         ],
-        dtype=np.float64,
+        dtype=np.complex128,
     )
     print("\n", stars_to_states(result))
