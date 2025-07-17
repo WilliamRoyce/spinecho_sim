77ab411cff707ac53320c4f1837a251bf5ed7dd2
William Royce <wr286@cam.ac.uk>
2025-07-17 11:38:04 +0000

    Majorana trajectories first implementation

--- spinecho_sim/majorana/__init__.py
+++ spinecho_sim/majorana/__init__.py
@@ -3,15 +3,17 @@
 from __future__ import annotations
 
 from spinecho_sim.majorana._companion_helper import (
-    majorana_stars_new,
+    majorana_stars,
 )
 from spinecho_sim.majorana._majorana import (
     majorana_stars_old,
+    simulate_trajectories_majorana,
     stars_to_states,
 )
 
 __all__ = [
-    "majorana_stars_new",
+    "majorana_stars",
     "majorana_stars_old",
+    "simulate_trajectories_majorana",
     "stars_to_states",
 ]
