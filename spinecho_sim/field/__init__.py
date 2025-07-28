cd1b56ceea2341b92093f1f7173dd846c7b0694e
William Royce <wr286@cam.ac.uk>
2025-07-28 13:16:37 +0000

    plot updates

---
+++ spinecho_sim/field/__init__.py
@@ -0,0 +1,11 @@
+"""Module for constructing and simulating magnetic fields."""
+
+from __future__ import annotations
+
+from spinecho_sim.field._field import (
+    CosSolenoid,
+)
+
+__all__ = [
+    "CosSolenoid",
+]
